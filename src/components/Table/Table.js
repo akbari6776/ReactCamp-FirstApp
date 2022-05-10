f45fdd5186da4a00de90d95eb0627f499c713238
mohammad akbari <cisco@mohammads-MacBook-Pro.local>
2022-05-10 16:06:30 +0430

    create table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,27 @@
 import React, {useState} from 'react'
-import TableHeader from './THeader/TableHeader'
+import TableStyle from './TableStyle.css'
+import { TList } from './TitleList'
 
 
 export default function Table(){
     return(
-        <TableHeader />
+        // <TableHeader />
+        <table className="table">
+        <tr>
+            {
+                TList.map(( item, i ) => {
+                    return <th key={i}>{item}</th>
+                })
+            }
+        </tr>
+        <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td className="edit-buttons"><button className="edit">Edit</button><button className="delete">Delete</button></td>
+        </tr>
+        </table>
     )
 }
